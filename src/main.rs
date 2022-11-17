d923ead8e9e019e50dab543e9dbfe06d1f89acde
Alexander <suguangqiang@popmart.com>
2022-11-17 15:42:15 +0800

    rust语言汇编指令生成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_sum());
+}
+fn get_sum() -> i32{
+    let mut sum = 0;
+    for i in 1..=100 {
+        sum += i
+    }
+    return sum
 }
