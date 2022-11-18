3d269882bca28c4bd99cd1d72cc7545e2ba0afdd
Alexander <suguangqiang@popmart.com>
2022-11-18 12:31:53 +0800

    格式化代码

--- src/learn_owner/mod.rs
+++ src/learn_owner/mod.rs
@@ -1,21 +1,1 @@
-// mod learn_owner;
-pub  fn owner_with_block(){
-    let s :&str ="hell world";
-    println!("s value = {s}");
-    {
-        let s:&str ="command line heroes!";
-        println!("s value ={s}")
-    }
-    println!("s value = {s}");
-}
-
-/**
- *   @see String#push_str(&mut self, string: &str)
-*/
-pub fn owner_whit_string(){
-    let mut  s:String = String::from("hello ");
-
-    s.push_str("command line heroes!");
-    println!("s value ={}",s)
-
-}
+// mod learn_owner;pub  fn owner_with_block(){    let s :&str ="hell world";    println!("s value = {s}");    {        let s:&str ="command line heroes!";        println!("s value ={s}")    }    println!("s value = {s}");}/** *   @see String#push_str(&mut self, string: &str)*/pub fn owner_whit_string(){    let mut  s:String = String::from("hello ");    s.push_str("command line heroes!");    println!("s value ={}",s)}
